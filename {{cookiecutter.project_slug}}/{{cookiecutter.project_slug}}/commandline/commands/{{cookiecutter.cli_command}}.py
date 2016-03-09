3a2eb01a325578843a77b18c41405dd55df34fde
Pablo Andres Fuente <pablo.a.fuente@intel.com>
2016-03-09 12:02:38 -0300

    Fixing the CLI command template
    Fixing some issues related to CLI commands with _ in their names.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/commandline/commands/{{cookiecutter.cli_command}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/commandline/commands/{{cookiecutter.cli_command}}.py
@@ -3,12 +3,12 @@ import cliff
 from {{ cookiecutter.project_slug }}.mixins import logger
 
 
-class {{ cookiecutter.cli_command.replace('-', '').replace('_', '') }}Command(cliff.command.Command, logger.WithLogger):
+class {{ cookiecutter.cli_command.replace('_', '').capitalize() }}Command(cliff.command.Command, logger.WithLogger):
     def get_description(self):
         return "{{ cookiecutter.cli_command }} command"
 
     def get_parser(self, prog_name):
-        parser = super({{ cookiecutter.cli_command.replace('-', '').replace('_', '') }}Command, self).get_parser(prog_name)
+        parser = super({{ cookiecutter.cli_command.replace('_', '').capitalize() }}Command, self).get_parser(prog_name)
         parser.add_argument('aParameter', nargs='?', default='.')
         return parser
 
